1cdd2b7c723d361f2fb4d924b29615926055b0cb
unknown <alejandrosv3@gmail.com>
2018-11-05 16:31:54 +0100

    ahora se puede vaciar la maquina y ver todo el dinero que habia

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -85,4 +85,12 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         return cantidadDeDineroADevolver;
     } 
+    
+    public int vaciarDineroDeLaMaquina() {
+        int dineroAVaciar;
+        dineroAVaciar = totalDineroAcumulado + balanceClienteActual;
+        totalDineroAcumulado = 0;
+        balanceClienteActual = 0;
+        return dineroAVaciar;
+    }
 }
